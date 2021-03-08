029f4d0a8f132d1182aaf841f849cadf9e46e54b
vaniisgh <vanisingh@live.co.uk>
2021-03-08 18:25:07 +0530

    notifications server

--- admin/resource_helper.py
+++ admin/resource_helper.py
@@ -7,9 +7,9 @@ from config import JWT_SECRET_KEY
 def triggerSocketioNotif(admin_id, token):
     sio = socketio.Client()
 
-    token = jwt.encode({"id": admin_id}, JWT_SECRET_KEY,
+    token = jwt.encode({'identity': {"email": admin_id}}, JWT_SECRET_KEY,
                        algorithm='HS256')
-    sio.connect('http://localhost:8008/', headers={
+    sio.connect('http://localhost:8008/', args={
                 "Authorization": token})
     sio.wait()
     sio.emit('message', {'admin_id': admin_id})
