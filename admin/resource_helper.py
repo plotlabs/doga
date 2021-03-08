ed69c3b1b9fb900f750e9f864cdedfc3340782c3
vaniisgh <vanisingh@live.co.uk>
2021-03-08 17:04:18 +0530

    algorithm update for jwt

--- admin/resource_helper.py
+++ admin/resource_helper.py
@@ -8,7 +8,7 @@ def triggerSocketioNotif(admin_id, token):
     sio = socketio.Client()
 
     token = jwt.encode({"id": admin_id}, JWT_SECRET_KEY,
-                       algorithm='HS512')
+                       algorithm='HS256')
     sio.connect('http://localhost:8008/', headers={
                 "Authorization": token})
     sio.wait()
